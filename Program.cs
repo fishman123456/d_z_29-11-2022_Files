f47dfa833c1f05004e1ddd7c4dfcc19938a404f3
denis <106389581+fishman123456@users.noreply.github.com>
2022-11-29 17:48:25 +0700

    1
    Нужно добавить исключения,
    если файл существует то выводим на экран содержимое

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@
 // Создать метод который создает файл, если такой файл уже существует, 
 // то сообщает что он есть. Создать метод, который считвыает 
 // инфомацию из файла и выводит её на экран.
+using System.Text;
+
 using static System.Console;
 
 // текущий каталог
@@ -11,27 +13,37 @@ WriteLine($"Full path to thedirectory:\n{ dir.FullName}");
 WriteLine($"Time of creation:{ dir.CreationTime}");
 WriteLine("\n\tAll directory files:");
 FileInfo[] files = dir.GetFiles(); // все файлы
+
+try
+{
+
+
+string filePath = "test.txt";
+using (FileStream fs = new FileStream(filePath, FileMode.CreateNew))
+{
+    using (StreamWriter sw = new StreamWriter(fs,Encoding.Unicode))
+
+    {
+        WriteLine("Введите что записываем");
+        string writeText = ReadLine();
+        // записываем данные в файл
+        sw.WriteLine(writeText);
+        foreach (var item in writeText)
+        {
+            sw.Write($"{item}");
+        }
+        WriteLine("\nДанные записаны!");
+    }
+}
+}
+catch (Exception)
+{
+
+    throw;
+}
 // в каталоге
 foreach (FileInfo f in files)
 {
-    WriteLine(f.Directory + "    " + f.Name + ":\t\t\t" + "\t"+f.Length);
-    
+    WriteLine(f.Name + ":\t\t\t" + "\t" + f.Length);
 }
-WriteLine();
-
-StreamWriter sw = null;
-try
-{
-    sw = new StreamWriter("file.txt");
-
-    for (int i = 0; i < 2; ++i)
-        sw.Write(i + "  Hello world!\n");
-}
-catch (Exception ex)
-{
-    Console.WriteLine("Output stream error");
-}
-finally
-{
-    if (sw != null) sw.Close();
-}
+WriteLine("------------------------------------------");
